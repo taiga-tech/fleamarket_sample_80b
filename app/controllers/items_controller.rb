b557040a9a39d2df29b9cea75a2e2e18541d9c29
noritanyutan <67891057+noritanyutan@users.noreply.github.com>
2020-08-09 15:22:13 +0900

    noritomi-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,17 +16,18 @@ class ItemsController < ApplicationController
   #商品情報
   def create
     @item = Item.new(item_params)
-  # if @item.save
-  # else
-    # render :new
-  # end
+  if @item.save 
+    redirect_to root_path
+  else
+    render :new
+  end
   end
   
   #商品編集
   def edit
   end
   
-  #商品あげるやつ
+  #商品更新機能
   def update
     # if @item.update(product_params)
     #   redirect_to root_path
@@ -43,7 +44,7 @@ class ItemsController < ApplicationController
 
   #ストロングパラメーター
   private
-  def product_params
+  def item_params
     params.require(:item).permit(
       :title,
       :price,
