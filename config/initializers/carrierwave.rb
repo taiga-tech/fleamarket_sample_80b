0adb293eb382ab5610d34a22bce0aca6ede2be3c
taiga-tech <taiga.dev.n@gmail.com>
2020-08-10 18:13:27 +0900

    update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'バケット名'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/バケット名'
+    config.fog_directory  = 'fleamarket80bb'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket80bb'
   end
 end
