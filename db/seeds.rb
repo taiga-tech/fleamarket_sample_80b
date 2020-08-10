b8abc16149f23e98041cef95d64249e870548d6c
noritanyutan <67891057+noritanyutan@users.noreply.github.com>
2020-08-10 23:31:31 +0900

    update_複数投稿

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,6 @@ CSV.foreach("db/csv/deliveries-table.csv") do |row|
   Delivery.create!(name: row[1], size: row[2], price: row[3])
 end
 
-Item.create!(title: "test10", price: 400, text: "test10", stock: 10, brand: "test3", condition: "test10",leadtime: 10, user_id: 10, delivery_id: 10, category_id: 10)
-User.create!(name: "test10", email: "testtest10@test", password: "testtest10", password_confirmation: "testtest10")
+# Item.create!(title: "test10", price: 400, text: "test10", stock: 10, brand: "test3", condition: "test10",leadtime: 10, user_id: 10, delivery_id: 10, category_id: 10)
+# User.create!(name: "test10", email: "testtest10@test", password: "testtest10", password_confirmation: "testtest10")
 
