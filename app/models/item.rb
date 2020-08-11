dd9cbd24aedd79e32e11d8d698ad434a10545e33
noritanyutan <67891057+noritanyutan@users.noreply.github.com>
2020-08-11 11:32:38 +0900

    update_noritomi_syuppinn_function

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
   has_many :comments 
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
+  validates :images, length: { minimum: 1, maximum: 10 }
+  validates :title, 
+            :price, 
+            :text, 
+            :stock, 
+            :condition, 
+            :leadtime, 
+            :category_id, 
+            :user_id, 
+            :delivery_id, 
+            presence: true
 end
