9151a5fcc05a424d552dddae6f4dbfe369671ac0
ishken141 <ishikenken141@icloud.com>
2020-08-11 21:35:47 +0900

    repair

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def show  
     user = User.find(params[:id]) 
     @name = user.name 
-    @items = user.items
+    @items = user.items  
+    # @zipcode = user.zipcode 
+    # @area = user.area 
+    # @city = user.city 
+    # @street = user.street 
   end 
 end
