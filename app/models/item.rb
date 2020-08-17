e723bb8eaf99cee2e259c32d6cf9d37e482bdca4
taiga-tech <taiga.dev.n@gmail.com>
2020-08-17 12:42:40 +0900

    fix destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many   :buys#, dependent: :destroy
   has_many   :comments#, dependent: :destroy
-  has_many   :images, dependent: :destroy 
-  has_many   :likes 
-  has_many   :liked_users, through: :likes, source: :user 
+  has_many   :images, dependent: :destroy
+  has_many   :likes, dependent: :destroy
+  has_many   :liked_users, through: :likes, source: :user
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :images, length: { minimum: 1, maximum: 10 }
   validates :title,
@@ -18,12 +18,13 @@ class Item < ApplicationRecord
             :category_id,
             :user_id,
             :delivery_id,
-            presence: true 
-  def self.search(search)  
-    if search 
-      Item.where('title LIKE(?)', "%#{search}%") 
-    else 
-      redirect_to root_path 
-    end 
-  end  
-end 
+            presence: true
+  def self.search(search)
+    if search
+      Item.where('title LIKE(?)', "%#{search}%")
+    else
+      redirect_to root_path
+    end
+  end
+end
+  
