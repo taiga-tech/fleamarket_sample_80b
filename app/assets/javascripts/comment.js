c1edc4f2c4890351a1229f7e6703a3efdd93ab14
taiga-tech <taiga.dev.n@gmail.com>
2020-08-20 14:07:57 +0900

    fix profile image

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,60 +1,56 @@
 $(function(){
-  function buildHTML(comment){   
-    if ( comment.image !== true) { 
-      var html = `
-        <div class="MessageInfo__userimage"> 
-        <img src="/assets/default.png" width="50px" height="50px" style="border-radius: 50%;">  
+  function buildHTML(comment){
+    if ( comment.image == "default.png") {
+      var html =
+        `<div class="MessageInfo__userimage">
+          <img src="/assets/default.png" width="50px" height="50px" style="border-radius: 50%;">
         </div>
         <div class="MessageBox">
-    <div class="Message">
-    <h1>${comment.text}</h1>
-    </div>
-    <div class="MessageInfo">
-    <div class="MessageInfo__userName">
-    <p>${comment.user_name}</p> 
-    </div>
-    </div>
-    </div> 
-    </div> 
-    `
-      return html; 
+          <div class="Message">
+            <h1>${comment.text}</h1>
+          </div>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              <p>${comment.user_name}</p>
+            </div>
+          </div>
+        </div>`
+      return html;
     }
-    else { 
-      var html = `
-      <div class="MessageInfo__userimage"> 
-      <img src="${comment.image_url} width="50px" height="50px" style="border-radius: 50%;"> 
-      </div>
-      <div class="MessageBox">
-  <div class="Message">
-  <h1>${comment.text}</h1>
-  </div>
-  <div class="MessageInfo">
-  <div class="MessageInfo__userName">
-  <p>${comment.user_name}</p> 
-  </div>
-  </div>
-  </div> 
-  </div> 
-  ` 
-  return html; 
-  }; 
+    else {
+      var html =
+        `<div class="MessageInfo__userimage">
+          <img src="${comment.image}" width="50px" height="50px" style="border-radius: 50%;">
+        </div>
+        <div class="MessageBox">
+          <div class="Message">
+            <h1>${comment.text}</h1>
+          </div>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              <p>${comment.user_name}</p>
+            </div>
+          </div>
+        </div>`
+  return html;
+  };
 }
 
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({ 
+    var url = $(this).attr('action');
+    $.ajax({
       url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
-    .done(function(data){ 
-      var html = buildHTML(data); 
-      $('.MessageField').append(html); 
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.MessageField').append(html);
       $('.form-area').val('');
       $('.submit').prop('disabled', false);
     })
@@ -63,4 +59,3 @@ $(function(){
     })
   })
 });
-
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -1,6 +1,4 @@
 json.text  @comment.text
 json.user_id  @comment.user.id
-json.user_name  @comment.user.name 
-json.image_url  @comment.user.profile.image.url 
-json.image @comment.user.profile.image.url 
-
+json.user_name  @comment.user.name
+json.image @comment.user.profile.image.url
