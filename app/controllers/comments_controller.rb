44fccb04fd668e13130e9264515c967f078901d5
ishken141 <ishikenken141@icloud.com>
2020-08-21 13:11:29 +0900

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,12 @@ end
   def destroy  
     @item = Item.find(params[:item_id]) 
     @comment = @item.comments.find(params[:id]) 
-    @comment.destroy 
-    redirect_to item_path(@item)
+    if @comment.destroy 
+    redirect_to item_path(@item) 
+    else 
+    redirect_to item_path(@item)  
+    end
   end 
-  
   private  
   def comment_params 
     params.require(:comment).permit(:text, :item_id, :user_id).merge(user_id: current_user.id, item_id: params[:item_id])
