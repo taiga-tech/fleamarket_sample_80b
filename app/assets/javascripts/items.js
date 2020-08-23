36b86751363c9c59129cb4c5204c01256b8d3e41
noritanyutan <67891057+noritanyutan@users.noreply.github.com>
2020-08-23 23:06:31 +0900

    配送料機能の実装　その２

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -113,7 +113,7 @@ $(function(){
   $("#item_price").on('keyup', function(){
     var price = $("#item_price").val();
     if( 300 <= price && price <= 9999999) {
-      var fee = Math.floor(price *0.03);
+      var fee = Math.floor(price * 0.03);
       var profit = (price - fee);
       $(".fee-span").text(fee);
       $(".profit-span").text(profit);
